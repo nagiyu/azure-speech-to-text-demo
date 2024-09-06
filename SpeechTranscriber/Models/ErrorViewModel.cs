54781d0e4603826c669892203fc403bbbb8a5172
nagiyu <otonagiyu@gmail.com>
2024-09-06 16:10:05 +0900

    Add initial project files and configuration

---
+++ SpeechTranscriber/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpeechTranscriber.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
